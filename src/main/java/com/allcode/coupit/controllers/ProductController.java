ec672d68a1b040d5948caf8a17c963632f2eed8b
Anderson Alvarez <andersonav91@gmail.com>
2018-04-06 19:07:55 -0500

    Rest service for creation of products and users

--- src/main/java/com/allcode/coupit/controllers/ProductController.java
+++ src/main/java/com/allcode/coupit/controllers/ProductController.java
@@ -4,8 +4,13 @@ import com.allcode.coupit.models.Product;
 import com.allcode.coupit.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/products")
@@ -16,4 +21,16 @@ public class ProductController {
 
     @GetMapping
     public Iterable<Product> getProduct(){ return productRepository.findAll(); }
+
+    @PostMapping
+    public ResponseEntity<?> addProduct(@RequestBody Product product) {
+        Product savedProduct = productRepository.save(product) ;
+        if(savedProduct.getId().equals(null))
+        {
+            return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+        }
+        else{
+            return new ResponseEntity<Product>(savedProduct, HttpStatus.CREATED);
+        }
+    }
 }
--- src/main/java/com/allcode/coupit/controllers/UserController.java
+++ src/main/java/com/allcode/coupit/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.allcode.coupit.controllers;
 
+import com.allcode.coupit.models.Product;
 import com.allcode.coupit.models.User;
 import com.allcode.coupit.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/users")
@@ -16,4 +18,16 @@ public class UserController {
 
     @GetMapping
     public Iterable<User> getUsers(){ return userRepository.findAll(); }
+
+    @PostMapping
+    public ResponseEntity<?> addUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user) ;
+        if(savedUser.getId().equals(null))
+        {
+            return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+        }
+        else{
+            return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+        }
+    }
 }
