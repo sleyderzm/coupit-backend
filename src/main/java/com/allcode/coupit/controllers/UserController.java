f5ab2193858b3dd0f845a427d45debbc66b0a4b0
Anderson Alvarez <andersonav91@gmail.com>
2018-04-10 11:25:31 -0500

    Added the user edition service, fix bugs for user validation

--- src/main/java/com/allcode/coupit/controllers/UserController.java
+++ src/main/java/com/allcode/coupit/controllers/UserController.java
@@ -11,13 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import com.allcode.coupit.handlers.ErrorResponse;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/users")
@@ -44,7 +47,8 @@ public class UserController {
         @RequestParam(value = "repeat_password", required=true) final String repeatPassword,
         @RequestParam(value = "role_id", required=true) final long roleId
     ) {
-        List<String> errors = this.validateUser(email, password, repeatPassword, roleId);
+        String[] fieldsToValidate = new String[] { "email","password", "role_id" };
+        List<String> errors = this.validateUser(roleId, email, password, repeatPassword, fieldsToValidate);
         if(errors.size() == 0){
             Role userRole = roleRepository.findById(roleId).get();
 
@@ -53,7 +57,8 @@ public class UserController {
 
             if(savedUser.getId().equals(null))
             {
-                return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+                ErrorResponse error = new ErrorResponse("Error when saving the user");
+                return new ResponseEntity<ErrorResponse>(error, HttpStatus.BAD_REQUEST);
             }
             else{
                 return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
@@ -63,10 +68,42 @@ public class UserController {
             ErrorResponse errorResponse = new ErrorResponse(String.join(", ", errors));
             return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
         }
-
     }
 
-    public List<String> validateUser(String email, String password, String repeatPassword, long roleId){
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editUser(
+            @PathVariable(name="id", required=true) Long id,
+            @RequestParam(value = "first_name", required=true) final String firstName,
+            @RequestParam(value = "last_name", required=true) final String lastName,
+            @RequestParam(value = "role_id", required=true) final long roleId
+    ) {
+        String[] fieldsToValidate = new String[] { "email" };
+        List<String> errors = this.validateUser(roleId, null, null, null, fieldsToValidate);
+        if(errors.size() == 0){
+            Role userRole = roleRepository.findById(roleId).get();
+
+            User user = userRepository.findById(id).get();
+
+            if(user.getId().equals(null))
+            {
+                ErrorResponse error = new ErrorResponse("User with ID " + id + " not found");
+                return new ResponseEntity<ErrorResponse>(error, HttpStatus.BAD_REQUEST);
+            }
+            else{
+                user.setFirstName(firstName);
+                user.setFirstName(lastName);
+                user.setRole(userRole);
+                User savedUser = userRepository.save(user);
+                return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+            }
+        }
+        else{
+            ErrorResponse errorResponse = new ErrorResponse(String.join(", ", errors));
+            return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public List<String> validateUser(long roleId, String email, String password, String repeatPassword, String[] fieldsToValidate){
         List<String> errors = new ArrayList<>();
 
         final Pattern valid_email_regex =
@@ -74,18 +111,23 @@ public class UserController {
 
         Matcher matcher = valid_email_regex.matcher(email);
 
-        if(! matcher.find()){
+        if(! matcher.find() && Arrays.asList(fieldsToValidate).contains("email")){
             errors.add("Email is not valid");
         }
 
-        if(! password.equals(repeatPassword)){ errors.add("Passwords do not match"); }
+        if(! password.equals(repeatPassword) && Arrays.asList(fieldsToValidate).contains("password")){ errors.add("Passwords do not match"); }
 
         User repeatUser = userRepository.findByEmail(email);
 
-        if (repeatUser != null) { errors.add("Email " + email + " already exist"); }
+        if (repeatUser != null && Arrays.asList(fieldsToValidate).contains("email")) { errors.add("Email " + email + " already exist"); }
 
-        try{ Role userRole = roleRepository.findById(roleId).get(); }
-        catch (Exception ex){ ErrorResponse error = new ErrorResponse("Role not exists"); }
+        try{
+            Role userRole = roleRepository.findById(roleId).get();
+            if (userRole.equals(null) && Arrays.asList(fieldsToValidate).contains("role_id")){
+                errors.add("Role not exists");
+            }
+        }
+        catch (Exception ex){ if(Arrays.asList(fieldsToValidate).contains("role_id")){ errors.add("Role not exists"); } }
 
         return errors;
     }
