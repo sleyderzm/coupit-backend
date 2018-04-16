2572a96158ee6f9581a1eb411c01244eb62f9e43
Sleyder Zuleta <sleyder@mobileaws.com>
2018-04-16 12:22:57 -0500

    currency consume neo api

--- src/main/java/com/allcode/coupit/controllers/CurrencyController.java
+++ src/main/java/com/allcode/coupit/controllers/CurrencyController.java
@@ -2,6 +2,7 @@ package com.allcode.coupit.controllers;
 
 import com.allcode.coupit.handlers.ErrorResponse;
 import com.allcode.coupit.handlers.MessageResponse;
+import com.allcode.coupit.handlers.Utils;
 import com.allcode.coupit.models.Blockchain;
 import com.allcode.coupit.models.Currency;
 import com.allcode.coupit.repositories.BlockchainRepository;
@@ -33,9 +34,11 @@ public class CurrencyController {
 
     @PostMapping(path="/update", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> updateCurrencies() {
-        //TODO: capture json from NEO API
-        String json = "{\"current_height\":3632,\"message\":\"\",\"page\":0,\"page_len\":500,\"results\":[{\"block\":3574,\"contract\":{\"author\":\"AndresJaramillo\",\"code\":{\"hash\":\"0xcb9f3b7c6fb1cf2c13a40637c189bdd066a272b4\",\"parameters\":\"0710\",\"returntype\":5,\"script\":\"\"},\"code_version\":\"0.0.1\",\"description\":\"CoupitSC\",\"email\":\"jandresjc@gmail.com\",\"name\":\"Coupit\",\"properties\":{\"dynamic_invoke\":false,\"storage\":true},\"version\":0},\"token\":{\"contract_address\":\"AMfXUmH4G9wTjdBzeB73Y936ocE4D5xrPj\",\"decimals\":8,\"name\":\"Coupit\",\"script_hash\":\"0xdb6cabefe8ae4a5e9f1ecc31b1f7ec00766c9d40\",\"symbol\":\"COU\"},\"tx\":\"0xbfe05958b7b343b3394c51933d58ba9116b0ef7e26d3e58f405fcdae0b459b82\",\"type\":\"SmartContract.Contract.Create\"}],\"total\":1}"; 
-        JSONObject response = new JSONObject(json);
+
+        JSONObject response = Utils.readJsonFromUrl(System.getenv("NEO_API") + "/v1/tokens");
+        if(response == null){
+            ErrorResponse error = new ErrorResponse("Error while trying to get the currencies");
+        }
 
         JSONArray results = response.getJSONArray("results");
         Blockchain blockchain = blockchainRepository.findByName(Blockchain.NEO);
